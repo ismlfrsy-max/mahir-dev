6a03edad7ecc57e7cc923f064342f719211e650c
ismail hasan <ismlfrsy@gmail.com>
2026-08-03 21:31:28 +0700

    Add files via upload

---
+++ terminal/anatomi jendelaterminal.sh
@@ -0,0 +1,70 @@
+PS C:\Users\Ismail Hasan\Documents\terminal> cd
+PS C:\Users\Ismail Hasan\Documents\terminal> hostname
+IHSAN
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop REM Perintah baru
+Set-Location : A positional parameter cannot be found that accepts argument 'REM'.
+At line:1 char:1
++ cd Desktop REM Perintah baru
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd anatomi jendelaterminal.sh
+Set-Location : A positional parameter cannot be found that accepts argument 'jendelaterminal.sh'
+.
+At line:1 char:1
++ cd anatomi jendelaterminal.sh
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal>                                                     
+PS C:\Users\Ismail Hasan\Documents\terminal> cd                                                 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd ..
+PS C:\Users\Ismail Hasan\Documents> cd ..
+PS C:\Users\Ismail Hasan> cd Desktop
+PS C:\Users\Ismail Hasan\Desktop> 
---
+++ terminal/rem-sample.sh
@@ -0,0 +1,4 @@
+C:\Users\NamaPengguna>hostname   REM Ini perintah yang diketik, 'hostname'
+NAMA-KOMPUTER-WINDOWS            REM Ini output dari perintah 'hostname'
+C:\Users\NamaPengguna>cd Desktop REM Perintah baru
+C:\Users\NamaPengguna\Desktop>_  REM Prompt berubah nunjukin direktori baru, siap lagi
