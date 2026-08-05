ea9dfed28bdf340bbeeb005cebecbbef6be47b99
ismail hasan <ismlfrsy@gmail.com>
2026-08-05 21:25:59 +0700

    Add files via upload

---
+++ anatomi jendelaterminal.sh
@@ -0,0 +1,348 @@
+PS C:\Users\Ismail Hasan\Documents\terminal> cd
+PS C:\Users\Ismail Hasan\Documents\terminal> hostname
+IHSAN
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop REM Perintah baru
+Set-Location : A positional parameter cannot be found that accepts argument 'REM'.
+At line:1 char:1
++ cd Desktop REM Perintah baru
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd anatomi jendelaterminal.sh
+Set-Location : A positional parameter cannot be found that accepts argument 'jendelaterminal.sh'
+.
+At line:1 char:1
++ cd anatomi jendelaterminal.sh
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal>                                                     
+PS C:\Users\Ismail Hasan\Documents\terminal> cd                                                 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd ..
+PS C:\Users\Ismail Hasan\Documents> cd ..
+PS C:\Users\Ismail Hasan> cd Desktop
+PS C:\Users\Ismail Hasan\Desktop> 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd
+PS C:\Users\Ismail Hasan\Documents\terminal> hostname
+IHSAN
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop REM Perintah baru
+Set-Location : A positional parameter cannot be found that accepts argument 'REM'.
+At line:1 char:1
++ cd Desktop REM Perintah baru
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd anatomi jendelaterminal.sh
+Set-Location : A positional parameter cannot be found that accepts argument 'jendelaterminal.sh'
+.
+At line:1 char:1
++ cd anatomi jendelaterminal.sh
+    + CategoryInfo          : InvalidArgument: (:) [Set-Location], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetLoca 
+   tionCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd Desktop
+cd : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\Desktop' because it does not exi
+st.
+At line:1 char:1
++ cd Desktop
++ ~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...erminal\Desktop:String) [Set-L 
+   ocation], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal>                                                     
+PS C:\Users\Ismail Hasan\Documents\terminal> cd                                                 
+PS C:\Users\Ismail Hasan\Documents\terminal> cd ..
+PS C:\Users\Ismail Hasan\Documents> cd ..
+PS C:\Users\Ismail Hasan> cd Desktop
+PS C:\Users\Ismail Hasan\Desktop> 
+ *  History restored 
+
+PS C:\Users\Ismail Hasan\Documents\terminal> cd
+PS C:\Users\Ismail Hasan\Documents\terminal> Get-Location
+
+Path                                    
+----                                    
+C:\Users\Ismail Hasan\Documents\terminal
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> cd ..
+PS C:\Users\Ismail Hasan\Documents> cd ..
+PS C:\Users\Ismail Hasan> dir
+
+
+
+
+Mode                 LastWriteTime         Length Name                                          
+----                 -------------         ------ ----                                          
+d-----        27/07/2026     20.39                .copilot                                      
+d-----        09/01/2026     14.22                .Ld9VirtualBox                                
+d-----        17/07/2026     06.48                .thumbnails                                   
+d-----        03/11/2025     16.14                .vscode                                       
+d-r---        02/11/2025     12.34                Contacts                                      
+d-r---        17/07/2026     11.17                Desktop                                       
+d-r---        03/08/2026     20.48                Documents                                     
+d-r---        03/08/2026     21.05                Downloads                                     
+d-r---        02/11/2025     12.34                Favorites                                     
+d-r---        02/11/2025     12.34                Links                                         
+d-r---        02/12/2025     13.36                Music                                         
+d-r---        24/07/2026     21.04                Pictures                                      
+d-r---        02/11/2025     12.36                Searches                                      
+d-r---        06/11/2025     09.15                Videos                                        
+-a----        19/12/2025     13.24            815 .gitconfig                                    
+-a----        12/06/2026     20.43              0 .python_history                               
+
+
+PS C:\Users\Ismail Hasan> dir /p
+dir : Cannot find path 'C:\p' because it does not exist.
++ dir /p
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\p:String) [Get-ChildItem], ItemNotFoundExcept 
+   ion
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan> dir /w
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\w:String) [Get-ChildItem], ItemNotFoundExcept 
+   ion
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan> dir /a
+dir : Cannot find path 'C:\a' because it does not exist.
++ dir /a
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\a:String) [Get-ChildItem], ItemNotFoundExcept 
+   ion
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan> cd terminal
+cd : Cannot find path 'C:\Users\Ismail Hasan\terminal' because it does not exist.
+At line:1 char:1
++ cd terminal
++ ~~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail Hasan\terminal:String) [Set-Location], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.SetLocationCommand
+ 
+PS C:\Users\Ismail Hasan\Documents> cd terminal 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir /a      
+dir : Cannot find path 'C:\a' because it does not exist.
+At line:1 char:1
++ dir /a
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\a:String) [Get-ChildItem], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir         
+
+
+    Directory: C:\Users\Ismail Hasan\Documents\terminal
+
+
+----                 -------------         ------ ----                                                                                     
+-a----        03/08/2026     21.13           3187 anatomi jendelaterminal.sh                                                               
+-a----        03/08/2026     21.07              0 file terminal                                                                            
+-a----        03/08/2026     21.11              0 navigasifolderditerminal.sh                                                              
+-a----        03/08/2026     21.22            426 notes.txt                                                                                
+-a----        03/08/2026     21.21            285 rem-sample.sh                                                                            
+
+PS C:\Users\Ismail Hasan\Documents\terminal> dir /a
+dir : Cannot find path 'C:\a' because it does not exist.
+At line:1 char:1
++ dir /a
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\a:String) [Get-ChildItem], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+PS C:\Users\Ismail Hasan\Documents\terminal> dir /p                                                                                         
+dir : Cannot find path 'C:\p' because it does not exist.
+At line:1 char:1
++ dir /p
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\p:String) [Get-ChildItem], ItemNotFoundException
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir /p
+dir : Cannot find path 'C:\p' because it does not exist.
+At line:1 char:1
++ dir /p
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\p:String) [Get-ChildItem], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir /a
+At line:1 char:1
++ dir /a
++ ~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\a:String) [Get-ChildItem], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> pwd   
+Path                                    
+----                                    
+C:\Users\Ismail Hasan\Documents\terminal
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> md DataRahasia\Tahun2024\LaporanBulanan 
+
+    Directory: C:\Users\Ismail Hasan\Documents\terminal\DataRahasia\Tahun2024
+
+
+Mode                 LastWriteTime         Length Name                                                                                     
+----                 -------------         ------ ----                                                                                     
+d-----        05/08/2026     20.58                LaporanBulanan                                                                           
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> type nul > index.html
+type : Cannot find path 'C:\Users\Ismail Hasan\Documents\terminal\nul' because it does not exist.
+At line:1 char:1
++ type nul > index.html
++ ~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...ts\terminal\nul:String) [Get-Content], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetContentCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> type nul > style.html
+At line:1 char:1
++ type nul > style.html
++ ~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (C:\Users\Ismail...ts\terminal\nul:String) [Get-Content], ItemNotFoundException
+    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetContentCommand
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir
+
+    Directory: C:\Users\Ismail Hasan\Documents\terminal
+
+
+Mode                 LastWriteTime         Length Name                                                                                     
+----                 -------------         ------ ----                                                                                     
+d-----        05/08/2026     20.58                DataRahasia                                                                              
+-a----        03/08/2026     21.13           3187 anatomi jendelaterminal.sh                                                               
+-a----        03/08/2026     21.07              0 file terminal                                                                            
+-a----        05/08/2026     20.59              0 index.html                                                                               
+-a----        03/08/2026     21.11              0 navigasifolderditerminal.sh                                                              
+-a----        03/08/2026     21.22            426 notes.txt                                                                                
+-a----        03/08/2026     21.21            285 rem-sample.sh                                                                            
+-a----        05/08/2026     21.01              0 style.html                                                                               
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> echo. > README.md
+echo. : The term 'echo.' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the n
+At line:1 char:1
++ echo. > README.md
++ ~~~~~
+    + CategoryInfo          : ObjectNotFound: (echo.:String) [], CommandNotFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
+ 
+PS C:\Users\Ismail Hasan\Documents\terminal> dir
+
+
+    Directory: C:\Users\Ismail Hasan\Documents\terminal
+
+Mode                 LastWriteTime         Length Name                                                                                     
+----                 -------------         ------ ----                                                                                     
+d-----        05/08/2026     20.58                DataRahasia                                                                              
+-a----        03/08/2026     21.13           3187 anatomi jendelaterminal.sh                                                               
+-a----        03/08/2026     21.07              0 file terminal                                                                            
+-a----        05/08/2026     20.59              0 index.html                                                                               
+-a----        03/08/2026     21.11              0 navigasifolderditerminal.sh                                                              
+-a----        03/08/2026     21.22            426 notes.txt                                                                                
+-a----        03/08/2026     21.21            285 rem-sample.sh                                                                            
+-a----        05/08/2026     21.01              0 style.html                                                                               
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> New-Item -ItemType File skrip.js
+
+
+    Directory: C:\Users\Ismail Hasan\Documents\terminal
+
+
+Mode                 LastWriteTime         Length Name                                                                                     
+----                 -------------         ------ ----                                                                                     
+-a----        05/08/2026     21.02              0 skrip.js                                                                                 
+
+
+PS C:\Users\Ismail Hasan\Documents\terminal> cat rem-sample.sh
+C:\Users\NamaPengguna>hostname   REM Ini perintah yang diketik, 'hostname'
+NAMA-KOMPUTER-WINDOWS            REM Ini output dari perintah 'hostname'
+C:\Users\NamaPengguna>cd Desktop REM Perintah baru
+C:\Users\NamaPengguna\Desktop>_  REM Prompt berubah nunjukin direktori baru, siap lagi
+PS C:\Users\Ismail Hasan\Documents\terminal> 
